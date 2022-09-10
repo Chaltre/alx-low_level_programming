0642287c266c580016d036ab986f428ae935ac27
Chaltre <111004301+Chaltre@users.noreply.github.com>
2022-09-10 03:47:29 +0100

    comb3 commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
-* main - print if the number is postive, zero, or negative
-*
-* Description: using the main function
-* this program prints "Programming is positive, zero, or negative
-* Return: 0
-*/
+ * main - entry point
+ *
+ * Return: alway returns 0
+ *
+ */
 int main(void)
 {
 	int i;
@@ -16,17 +15,17 @@ int main(void)
 		for (j = 10; j <= 19; j++)
 		{
 			if ((j % 10) > (i % 10))
-			{	
+			{
 				putchar((i % 10) + '0');
 				putchar((j % 10) + '0');
- 				if (i != 18 || j != 19)
+				if (i != 18 || j != 19)
 				{
 					putchar(',');
 					putchar(' ');
 				}
-			}	
+			}
 
-		}	
+		}
 	}
 	putchar('\n');
 	return (0);
