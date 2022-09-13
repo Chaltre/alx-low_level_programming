4e2e58a62199c0f2a1b19bdfbdc6333c39fc6463
chaltre <chiamakaalichi@gmail.com>
2022-09-13 08:32:31 -0700

    task1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**   
+ * main - print the string in the put function 
+ *
+ * Description: using the main function     
+*/
+int main(void)
+{
+	printf("_putchar,\n");
+	return (0);
+}
+
