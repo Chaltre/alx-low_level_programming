44f3665c33a02b7ee7fec4543f334f525a5f5f7f
chaltre <chiamakaalichi@gmail.com>
2022-09-13 15:00:01 -0700

    task 0

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet - Prints the alphabet in lowercas * Return: Always 0 (Success)
+ * print_alphabet - Prints the alphabet in lowercase *  Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
