94c35bc4c341cb923d6c45cde26be50455f60c0d
chaltre <chiamakaalichi@gmail.com>
2022-09-14 10:58:36 -0700

    task5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 
 /**
  * print_sign - Determine if the input number is greater, equal or less than zero
- * @n: The input number as an integer.
+ * @n:The input number as an integer.
  * Return: 1 is greater than zero. 0 is zero.
  * -1 is less than zero.
  */
