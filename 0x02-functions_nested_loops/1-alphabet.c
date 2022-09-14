8fa6914e2bcd028da56ea347bbe4a6e508780df8
chaltre <chiamakaalichi@gmail.com>
2022-09-13 17:59:07 -0700

    task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,7 +4,7 @@
  *
  * Return: Always 0 (Success)
  */
-void print_alphabet(void)
+void print _alphabet(void)
 {
 	char c;
 
@@ -12,5 +12,5 @@ void print_alphabet(void)
 	{
 		_putchar(c);
 	}
-_putchar('\n');
+	_putchar('\n');
 }
