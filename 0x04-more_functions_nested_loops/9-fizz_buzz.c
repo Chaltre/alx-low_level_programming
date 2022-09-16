d1fd23dd75a3500a181fee8f6a9e0ed1ad5edc00
chaltre <chiamakaalichi@gmail.com>
2022-09-16 01:17:48 -0700

    yo

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,21 +7,24 @@
 
 int main(void)
 {
-	int i = 1;
+	int i;
 
-	while (i <= 100)
+	for (i = 1; i <= 100; i++)
 	{
-		if (i % 15 == 0)
-			printf("FizzBuzz ");
-		else if (i % 3 == 0)
-			printf("Fizz ");
-		else if (i % 5 == 0)
-			printf("Buzz ");
-		else
-		printf("%d ", i);
-		i++;
+	if (i % 3 == 0 && !(i % 5 == 0))
+		printf("Fizz");
+	else if (i % 5 == 0 && !(i % 3 == 0))
+		printf("Buzz");
+	else if (i % 3 == 0 && i % 5 == 0)
+		printf("FizzBuzz");
+	else
+		printf("%d", i);
+	if (i != 100)
+		printf(" ");
+	else
+		printf("\n");
 	}
-	printf("\n");
-
+	return (0);
 }
+
 
