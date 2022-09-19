7078b705911cdc97ab377a7a945cf079383d340b
chaltre <chiamakaalichi@gmail.com>
2022-09-19 13:05:57 -0700

    added files

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,9 +11,7 @@ void print_rev(char *s)
 	int i;
 
 	for (i = 0; s[i] != '\0'; i++)
-	{
-		for (--i; i >= 0; --i)
-			putchar(s[i]);
-	}
+	for (--i; i >= 0; --i)
+		putchar(s[i]);
 	putchar('\n');
 }
