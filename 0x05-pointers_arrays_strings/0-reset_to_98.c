eda5407d30ded748766fbe74acb2cadfacb7c673
chaltre <chiamakaalichi@gmail.com>
2022-09-19 01:43:22 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to - writing a function that takes int as a parameter
+ * @n: update the value it points to 98
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
