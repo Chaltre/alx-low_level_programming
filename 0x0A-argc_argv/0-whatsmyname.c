00137854d515cd77552d4255fea84c4994067747
Chaltre <chiamakaalichi@gmail.com>
2022-10-01 03:14:35 -0700

    task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - print the name of the file followed by a new line
+ * main - print the name of the file followed by a new line
+ * @argv: arguments passed to main as an array of strings
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
