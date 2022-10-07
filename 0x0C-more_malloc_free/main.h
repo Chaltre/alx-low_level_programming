f6240a8d6c37a16898a8f7f742e3807fde6d6ea8
Chaltre <chiamakaalichi@gmail.com>
2022-10-07 11:37:22 -0700

    task

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+
 int _putchar(char c);
 void *malloc(size_t size);
 void free(void *ptr);
@@ -11,4 +13,4 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
-#endif
+#endif /* MAIN_H */
