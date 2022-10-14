9cacdd59620300ec5133680d79ce76398e171a6a
Chaltre <chiamakaalichi@gmail.com>
2022-10-14 14:08:42 -0700

    task

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _isNum - check if is a number
+ * @num: string to check
+ * Return: 1 is numm, 0 not num
+ */
+int _isNum(char *num)
+{
+	int i;
+
+
